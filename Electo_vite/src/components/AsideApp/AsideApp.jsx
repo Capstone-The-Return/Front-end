eb16b5ce7b6c227a5f5dfff54f680dc34f8245bf
a.kesisi <esd25018@uom.edu.gr>
2025-11-28 21:57:52 +0200

    aside app delete changes

--- Electo_vite/src/components/AsideApp/AsideApp.jsx
+++ Electo_vite/src/components/AsideApp/AsideApp.jsx
@@ -1,28 +1,12 @@
 import style from "./AsideApp.module.css";
 import { Link } from "react-router-dom";
-import { FiHome, FiUser, FiSettings, FiLogOut } from "react-icons/fi";
 
 export default function AsideApp() {
   return (
     <aside className={style.container}>
-      <nav className={style.nav}>
-        <Link to="/home" className={style.link}>
-          <FiHome className={style.icon} />
-          <span>Αρχική</span>
-        </Link>
-        <Link to="/profile" className={style.link}>
-          <FiUser className={style.icon} />
-          <span>Προφίλ</span>
-        </Link>
-        <Link to="/settings" className={style.link}>
-          <FiSettings className={style.icon} />
-          <span>Ρυθμίσεις</span>
-        </Link>
-        <Link to="/logout" className={style.linkLogout}>
-          <FiLogOut className={style.icon} />
-          <span>Αποσύνδεση</span>
-        </Link>
-      </nav>
+      <p>aside content</p>
+      {/* Προαιρετικά μπορείς να βάλεις links */}
+      {/* <Link to="/somepath">Link example</Link> */}
     </aside>
   );
 }
