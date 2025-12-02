a1db220bda9306d148d28f5a1d5647c07a1e4d40
a.kesisi <esd25018@uom.edu.gr>
2025-12-02 23:46:22 +0200

    Add english labels

--- Electo_vite/src/components/AppBar/AppBar.jsx
+++ Electo_vite/src/components/AppBar/AppBar.jsx
@@ -4,9 +4,7 @@ import electronicsIcon from "../../assets/electronics_icon.png";
 export default function AppBar() {
   return (
     <header className={styles.container}>
-      <a href="/login" aria-label="Go to login page">
         <img src={electronicsIcon} className={styles.icon} alt="Electronics Icon" />
-      </a>
       <h1 className={styles.headertext}>Electronics Portal</h1>
     </header>
   );
