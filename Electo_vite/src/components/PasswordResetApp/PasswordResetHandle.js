f700c531853aae7257d38259e569ad56e36cee87
Stamalex <stamelosalexios@gmail.com>
2025-12-16 18:43:52 +0200

    imporved email service

--- Electo_vite/src/components/PasswordResetApp/PasswordResetHandle.js
+++ Electo_vite/src/components/PasswordResetApp/PasswordResetHandle.js
@@ -1,16 +1,12 @@
 import users from "../../../db/users.json";
-import { sendAnEmail } from "../../utilities/emailService";
+import { resetPasswordEmail } from "../../utilities/emailService";
 
 export function passwordResetHandle({ email }) {
   for (const user of users.users) {
     if (user.email === email) {
       // In a real application, here you would trigger the password reset email
-      sendAnEmail(
-        { email },
-        "Password Reset Request",
-        "Please reset your password using the following link: [reset link]"
-      );
-      console.log(`Password reset link sent to ${email}`);
+      resetPasswordEmail({ email }, "Password Reset Request", "testlink12345 ");
+
       return;
     }
 
