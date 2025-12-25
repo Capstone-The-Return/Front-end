96efbe49d7aa061e449abda654cf5556a07d65e8
a.kesisi <esd25018@uom.edu.gr>
2025-12-25 15:44:22 +0200

    delete navigation bar from main page

--- Electo_vite/src/pages/MainPage/MainPage.jsx
+++ Electo_vite/src/pages/MainPage/MainPage.jsx
@@ -2,7 +2,6 @@ import AppBar from "../../components/AppBar/AppBar";
 import FooterApp from "../../components/FooterApp/FooterApp";
 import MainPageApp from "../../components/MainPageApp/MainPageApp";
 import style from "./MainPage.module.css";
-import NavigationApp from "../../components/NavigationApp/NavigationApp";
 import Slider from "../../components/MainPageApp/Slider";
 import FormMain from "../../components/MainPageApp/FormMain";
 
@@ -11,10 +10,6 @@ export default function MainPage() {
     <>
       <AppBar />
       <div className={style.layout}>
-        <NavigationApp />
-
-
-
         <div className={style.mainContent}>
           <Slider />
           <MainPageApp />
